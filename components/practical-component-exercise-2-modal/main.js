baacd0743ff6126a40bdbfb440d0252d5995dff1
Renoir dos Reis <renofaria@gmail.com>
2017-12-09 20:22:59 -0200

    Laracasts Vue - Practical component exercise #2: Modal

---
+++ components/practical-component-exercise-2-modal/main.js
@@ -0,0 +1,22 @@
+/* jshint esversion:6 */
+
+Vue.component('modal', {
+  template: `
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+      <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
+  `,
+});
+
+var app = new Vue({
+  el: '#root',
+  data: {
+    showModal: false
+  },
+});
