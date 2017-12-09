4359a20b4f1b9cf242564672ca3b2a16d8957e1d
Renoir dos Reis <renofaria@gmail.com>
2017-12-09 20:20:32 -0200

    Laracasts Vue - Practical component Exercise #1: Messages

---
+++ components/practical-component-exercise-1-messages/main.js
@@ -0,0 +1,35 @@
+/* jshint esversion:6 */
+
+Vue.component('message', {
+  props: ['title', 'body'],
+
+  data(){
+      return {
+        isVisible: true
+      };
+  },
+
+  template: `
+    <article class="message" v-show="isVisible">
+      <div class="message-header">
+        <p>{{ title }}</p>
+        <button class="delete" aria-label="delete" @click="hideModal"></button>
+      </div>
+      <div class="message-body">
+        {{ body }}
+      </div>
+    </article>
+  `,
+
+  methods: {
+    hideModal(){
+      this.isVisible = false;
+    }
+  }
+});
+
+var app = new Vue({
+  el: '#root',
+  data: {
+  },
+});
