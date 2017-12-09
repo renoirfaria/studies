ca90c44f568c4c82e5deff2de0dda9f0903b182b
Renoir dos Reis <renofaria@gmail.com>
2017-12-09 20:23:21 -0200

    Laracasts Vue - Practical component exercise #3: Tabs

---
+++ components/practical-component-exercise-3-tabs/main.js
@@ -0,0 +1,77 @@
+/* jshint esversion:6 */
+
+Vue.component('tabs', {
+  template: `
+    <div>
+      <div class="tabs">
+        <ul>
+          <li v-for="tab in tabs" :class="{'is-active': tab.isActive }">
+            <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="tabs-details">
+        <slot></slot>
+      </div>
+    </div>
+
+  `,
+
+  data()
+  {
+    return {
+      tabs: [],
+    }
+  },
+
+  created() {
+    this.tabs = this.$children;
+  },
+
+  methods: {
+    selectTab(selectedTab){
+      this.tabs.forEach(tab => {
+        tab.isActive = (tab.name == selectedTab.name);
+      });
+    }
+  }
+});
+
+Vue.component('tab', {
+  props: {
+    name: { required: true },
+    selected: { default: false }
+  },
+
+  template: `
+    <div v-show="isActive" role="tab">
+      <slot></slot>
+    </div>
+  `,
+
+  data()
+  {
+      return {
+        isActive: false,
+      };
+  },
+
+  computed: {
+    href()
+    {
+        return '#' + this.name.toLowerCase().replace(/ /g, '-');
+    }
+  },
+
+  mounted()
+  {
+    this.isActive = this.selected;
+  }
+});
+
+var app = new Vue({
+  el: '#root',
+  data: {
+  },
+});
